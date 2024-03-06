34ed00ae6a72f7c4b04b3807a60d815e3aef4ee3
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-03-06 14:21:25 +0100

    route : categories and other routes

--- Evento/routes/web.php
+++ Evento/routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -38,3 +40,42 @@ Route::post('/login', [LoginController::class, 'store']);
 Route::post('/logout', [LogoutController::class, 'destroy'])->name('logout')->middleware('auth');
 
 
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+});
+
+Route::get('/allusers', function () {
+    return view('admin.allusers');
+});
+
+Route::get('/events', function () {
+    return view('admin.events');
+});
+
+Route::get('/approveEvents', function () {
+    return view('admin.approveEvents');
+});
+
+Route::get('/organisateur', function () {
+    return view('organiser.dashboard');
+});
+
+Route::get('/side', function () {
+    return view('admin.side');
+});
+
+
+//category
+Route::get('/categories', [CategoryController::class, 'showCategories']);
+
+Route::post('/categories', [CategoryController::class, 'store']);
+
+Route::delete('/deleteCategory/{id}', [CategoryController::class, 'destroyCategory']);
+
+//users amdin
+Route::get('/allusers', [UserController::class, 'show']);
+
+//organiser
+Route::get('/createEvent', function () {
+    return view('organiser.createEvent');
+});
