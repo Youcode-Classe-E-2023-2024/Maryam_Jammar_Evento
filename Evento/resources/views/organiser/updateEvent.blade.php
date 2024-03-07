c8de7a49aa886b81f8f11dda4e6492aff9956686
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-03-07 15:03:37 +0100

    modification de l'image

--- Evento/resources/views/organiser/updateEvent.blade.php
+++ Evento/resources/views/organiser/updateEvent.blade.php
@@ -11,7 +11,7 @@
                 <div class="flex items-center justify-center p-12">
                     <!-- Author: FormBold Team -->
                     <div class="mx-auto w-full max-w-[550px] bg-white">
-                        <form action="/updateEvent/{{$event->id}}" method="post">
+                        <form action="/updateEvent/{{$event->id}}" method="post" enctype="multipart/form-data">
                             @csrf
                             <div class="-mx-3 flex flex-wrap">
                                 <div class="w-full px-3 sm:w-1/2">
