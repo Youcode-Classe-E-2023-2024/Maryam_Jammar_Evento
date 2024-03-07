b83f73ff49d4ceebf4ba695b6f82faa3837c32eb
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-03-07 01:34:32 +0100

    modifier l'event

--- Evento/app/Http/Controllers/EventController.php
+++ Evento/app/Http/Controllers/EventController.php
@@ -57,17 +57,58 @@ class EventController extends Controller
             'creator' => $user,
         ]);
 
-        return redirect()->back();
+        return redirect('/allEvents');
     }
 
-    
+    /**
+     * Display the specified resource.
+     */
+    public function editEvent($id)
+    {
+        $content = file_get_contents('https://raw.githubusercontent.com/alaouy/sql-moroccan-cities/master/json/ville.json');
+        $data = json_decode($content);
+
+        $event = Event::find($id);
+
+        return view('organiser.updateEvent', compact('event', 'data'));
+    }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function updateEvent(Request $request, $id)
     {
-        //
+        $user = Auth::id();
+        $event = Event::findOrFail($id);
+
+        $request->validate([
+            'title' => 'required',
+            'location' => 'required',
+            'price' => 'required',
+            'date' => 'required',
+            'time' => 'required',
+            'description' => 'required',
+            'reservation_type' => 'required',
+            'image' => 'required',
+        ]);
+
+        Event::update([
+            'title' => $request->title,
+            'location' => $request->location,
+            'date' => $request->date,
+            'time' => $request->time,
+            'price' => $request->price,
+            'nbr_place' => $request->nbr_place,
+            'description' => $request->description,
+            'reservation_type' => $request->reservation_type,
+            'image' => $request->image,
+            'creator' => $user,
+        ]);
+
+        $event->creator = $user;
+
+        $event->save();
+        return redirect('/allEvents');
     }
 
     /**
